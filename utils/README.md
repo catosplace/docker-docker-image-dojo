b490fe2a4cf57a3a0d29c5a65146864b988214d8
Peter Sellars <psellars@gmail.com>
2020-11-28 18:22:51 +1300

    updated Docker/Dojo version, added Docker to pre-requisites install

---
+++ utils/README.md
@@ -0,0 +1,9 @@
+# Install Pre-requisites
+This script will install Docker, Dojo and pre-commit.
+
+You will need to run the script as an administrator due to some Docker installation permissions.
+
+## Run the installastion script
+```
+sudo ./install_prerequisites.sh
+```
--- utils/install_prerequisites.sh
+++ utils/install_prerequisites.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
 # Import .env variables
-read -ra args < <(grep -E -v '^#' .env | xargs)
+read -ra args < <(grep -E -v '^#' ../.env | xargs)
 export "${args[@]}"
+
+# Install Docker
+RUN set -eux; \
+    sudo systemctl stop docker; \
+    sudo systemctl stop containerd; \
+  	if ! wget -O docker.tgz "https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz"; then \
+		echo >&2 "error: failed to download 'docker-${DOCKER_VERSION}' from stable for x86_64"; \
+		exit 1; \
+	fi; \
+  	\
+	tar --extract \
+		--file docker.tgz \
+		--strip-components 1 \
+        --overwrite \
+		--directory /usr/bin/ \
+	; \
+	rm docker.tgz; \
+	\
+    sudo systemctl start docker; \
+    sudo systemctl start containerd; \
+	dockerd --version; \
+	docker --version
 
 # Install Dojo
 wget -O /tmp/dojo https://github.com/kudulab/dojo/releases/download/"${DOJO_VERSION}"/dojo_linux_amd64
